8668432249f6fca4d5800e4f8284937bb26d5931
長谷川一輝 <e3nr3hwtl.87269.yu@gmail.com>
2023-11-18 14:30:35 +0900

    スライダー途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
+//ページトップへ戻るボタン（スクロールして出てくるタイプ）
+var pagetop = $('#page-topbtn');
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    jQuery('.page-top').addClass( 'is-show' );
+  } else {
+    jQuery('.page-top').removeClass( 'is-show' );
+  }
+});
+
+pagetop.click(function () {
+  $('body, html').animate({ scrollTop: 0 }, 500);
+  return false;
+});
+
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   
@@ -47,6 +62,42 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         delay: 4000,
         disableOnInteraction: false,
     },
+  });
+
+  // リサイズ処理（PC時のみ矢印表示）
+  const service_slideLength = document.querySelectorAll('.js-campaign-swiper .swiper-slide').length
+  $(window).resize(function () {
+      service_arrow();
+  });
+  service_arrow();
+  function service_arrow() {
+      if (window.matchMedia('(max-width: 767px)').matches || service_slideLength <= 3) {
+          $('.js-service-arrow').hide();
+      } else {
+          $('.js-service-arrow').show();
+      }
+  }
+
+  // Swiper
+  var service_swiper = new Swiper(".js-campaign-swiper", {
+      loop: true,
+      speed: 2000,
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+      autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+      },
+      breakpoints: {
+          768: {
+              slidesPerView: 3.5,
+              spaceBetween: 40
+          }
+      },
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
   });
 
 });
