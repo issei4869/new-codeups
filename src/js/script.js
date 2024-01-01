754bc06f633260b42157a493060de48a273b9b76
長谷川一輝 <e3nr3hwtl.87269.yu@gmail.com>
2024-01-01 14:08:07 +0900

    モーダル実装中

--- src/js/script.js
+++ src/js/script.js
@@ -142,3 +142,21 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 });
 
+// ギャラリー画像モーダル表示イベント
+$(".js-gallery-list__item img").click(function () {
+  // まず、クリックした画像の HTML(<img>タグ全体)を#frayDisplay内にコピー
+  $("#grayDisplay").html($(this).prop("outerHTML"));
+  //そして、fadeInで表示する。
+  $("#grayDisplay").fadeIn(200);
+
+
+  return false;
+});
+
+// コース画像モーダル非表示イベント
+// モーダル画像背景 または 拡大画像そのものをクリックで発火
+$("#grayDisplay").click(function () {
+  // 非表示にする
+  $("#grayDisplay").fadeOut(200);
+  return false;
+});
